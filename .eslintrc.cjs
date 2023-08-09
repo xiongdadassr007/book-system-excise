0818c6c158b8633259eef9694d7ce91b9fa6fa16
xiongguangsen02 <3096429133@qq.com>
2023-08-09 21:06:31 +0800

    main 初始化项目配置

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+/*
+ * @Author: xiongguangsen02 3096429133@qq.com
+ * @Date: 2023-08-09 20:38:34
+ * @LastEditors: xiongguangsen02 3096429133@qq.com
+ * @LastEditTime: 2023-08-09 20:47:59
+ * @FilePath: \book-system-excise\.eslintrc.cjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        printWidth: 80,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index']
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'],
+    'no-undef': 'error'
+  }
+}
