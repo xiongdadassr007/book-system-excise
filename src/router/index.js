7e2223c0affde256dca6432ebc087801c22f3ab0
xiongguangsen02 <3096429133@qq.com>
2023-08-09 22:58:56 +0800

    main 删除模拟的错误代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-console.log(aaa)
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
