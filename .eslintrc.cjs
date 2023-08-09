01dda10ba7c1fe1a5dfaa9f1479b93a7d2db14ea
xiongguangsen02 <3096429133@qq.com>
2023-08-09 22:25:12 +0800

    main 删除错误代码再次提交

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,9 @@
-/*
- * @Author: xiongguangsen02 3096429133@qq.com
- * @Date: 2023-08-09 20:38:34
- * @LastEditors: xiongguangsen02 3096429133@qq.com
- * @LastEditTime: 2023-08-09 20:47:59
- * @FilePath: \book-system-excise\.eslintrc.cjs
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
