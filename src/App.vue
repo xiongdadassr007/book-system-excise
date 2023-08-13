98675a145ee5e44a82ba823f92896af34133fb1e
xiongguangsen <3096429133@qq.com>
2023-08-13 23:43:35 +0800

    main pinia规范管理1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup></script>
+<script setup>
+import { useUserloginData, useCounterData } from '@/store'
+const userloginData = useUserloginData()
+const counterData = useCounterData()
+</script>
 
 <template>
   <div>App</div>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <h2>{{ userloginData.token }}</h2>
+  <el-button @click="userloginData.setToken" type="success">登录</el-button>
+  <el-button @click="userloginData.clearToken" type="info">注销</el-button>
+  <h2>{{ counterData.counter }}</h2>
+  <el-button @click="counterData.addCounter" type="success">+2</el-button>
+  <el-button @click="counterData.reduceCounter" type="info">-2</el-button>
+  <test-need-component></test-need-component>
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import pinia from './store'
 import '@/assets/main.scss'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
